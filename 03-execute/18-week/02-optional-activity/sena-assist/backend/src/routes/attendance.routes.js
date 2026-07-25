bddfad9ee3ea9307daa01ace5f12e231c95f61b9
Jeison Fernando Joaqui Rodriguez <yeison56785@gmail.com>
2026-07-25 13:54:04 -0500

    Delete 03-execute/18-week/02-optional-activity/sena-assist directory

--- 03-execute/18-week/02-optional-activity/sena-assist/backend/src/routes/attendance.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { escanearQR } = require('../controllers/attendance.controller');
-
-router.post('/escanear', escanearQR);
-
-module.exports = router;
--- 03-execute/18-week/02-optional-activity/sena-assist/backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, perfil } = require('../controllers/auth.controller');
-const { verificarToken } = require('../middleware/auth');
-
-router.post('/login', login);
-router.get('/perfil', verificarToken, perfil);
-
-module.exports = router;
